211eb6fecaf4d3284c20eb6531f22cee2e2730e0
joshuab98 <jeb95@students.uwf.edu>
2022-03-05 23:03:45 -0600

    Adjusted AI, Should block moves

--- Connect4AI/src/connect4AI/Connect4AI.java
+++ Connect4AI/src/connect4AI/Connect4AI.java
@@ -10,17 +10,17 @@ import java.util.Random;
  */
 public class Connect4AI {
 	private int difficulty;// How many layers deep the AI will compute potential moves 
-	private int bestScore = 0;
+	//private int bestScore = 0;
 	private int score;
 	private int maxScore;
 	private int minScore;
-	//private int bestMove = -1;
+	private int bestMove = -1;
 	private int maxMove;
 	private int minMove;
 	private int playerOScore;
 	private int playerXScore;
 	public Connect4AI() {
-		difficulty = 3;
+		difficulty = 2; //CURRENTLY ONLY WORKS ON THIS DIFFICULTY
 
 	}
 	
@@ -32,182 +32,41 @@ public class Connect4AI {
 	public int score(Connect4Core game) { 
 		
 		//TODO tune this implementation or make a better one.
-		bestScore = 0;
+	    int bestScore = 0;
 		playerOScore = 0;
 		playerXScore = 0;
 		int[] rowArray = new int[game.getBoardSize()];
 		int [] colArray = new int[game.getBoardSize()];
 		/**
-		 * is supposed to gather score of all possible rows of 4, might be a better way to implement this
-		 * 4 in a row yields highest value 
-		 * 3 in a row has a high value but not as much as 4
-		 * 2 in a row has minimal value
-		 * Currently commented out because it makes the bot act very very dumb, comment ends at Line 204
+		 * simply gets a score if anyone is about to win and bases the move off of that.
 		 */
-		/**
+		for(int i = 0; i < game.getBoardSize(); i++) {
+			rowArray = game.getRow(i);
+			colArray = game.getCol(i);
+			//System.out.println(rowArray[i]  + "         ARRAYS AT i   =    " + i + "   "+ colArray[i]);
+		}
+		
 		if(game.checkForWin() == true && game.getCurrentPlayer() == 'O') {
-			playerOScore += 10000;
+			playerOScore += 1000000;
 		}
 		if(game.checkForWin() == true && game.getCurrentPlayer() == 'X') {
 			playerXScore += 10000;
 		}
-		for(int i = 0; i < game.getBoardSize() - 3; i++) { 
-			rowArray = game.getRow(i);
-			
-			if(rowArray[i] == 'O' && rowArray[i+1] == 'O' &&rowArray[i+2] == 'O' &&rowArray[i+3] == 'O') {
-				playerOScore += 10000;
-			}
-		    if (rowArray[i] == 'X' && rowArray[i+1] == 'X' &&rowArray[i+2] == 'X' &&rowArray[i+3] == 'X') {
-				playerXScore += 5000;
-			}
-			if (rowArray[i] == 'O' &&rowArray[i+2] == 'O' &&rowArray[i+3] == 'O' && rowArray[i+1] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (rowArray[i] == 'O' &&rowArray[i+1] == 'O' &&rowArray[i+3] == 'O' && rowArray[i+2] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (rowArray[i] == 'O' &&rowArray[i+1] == 'O' &&rowArray[i+2] == 'O' && rowArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (rowArray[i+1] == 'O' &&rowArray[i+2] == 'O' &&rowArray[i+3] == 'O' && rowArray[i+1] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (rowArray[i] == 'O' && rowArray[i+1] == 'O'&& rowArray[i+2] == Character.MIN_VALUE && rowArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (rowArray[i] == 'O' && rowArray[i+2] == 'O' && rowArray[i+1] == Character.MIN_VALUE && rowArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (rowArray[i] == 'O' && rowArray[i+3] == 'O' && rowArray[i+1] == Character.MIN_VALUE && rowArray[i+2] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (rowArray[i+1] == 'O' && rowArray[i+2] == 'O' && rowArray[i] == Character.MIN_VALUE && rowArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (rowArray[i+1] == 'O' && rowArray[i+3] == 'O' && rowArray[i] == Character.MIN_VALUE && rowArray[i+2] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (rowArray[i+2] == 'O' && rowArray[i+3] == 'O' && rowArray[i] == Character.MIN_VALUE && rowArray[i+1] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			
-
-			if (rowArray[i] == 'X' &&rowArray[i+2] == 'X' &&rowArray[i+3] == 'X' && rowArray[i+1] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (rowArray[i] == 'X' &&rowArray[i+1] == 'X' &&rowArray[i+3] == 'X' && rowArray[i+2] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (rowArray[i] == 'X' &&rowArray[i+1] == 'X' &&rowArray[i+2] == 'X' && rowArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (rowArray[i+1] == 'X' &&rowArray[i+2] == 'X' &&rowArray[i+3] == 'X' && rowArray[i+1] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (rowArray[i] == 'X' && rowArray[i+1] == 'X'&& rowArray[i+2] == Character.MIN_VALUE && rowArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (rowArray[i] == 'X' && rowArray[i+2] == 'X' && rowArray[i+1] == Character.MIN_VALUE && rowArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (rowArray[i] == 'X' && rowArray[i+3] == 'X' && rowArray[i+1] == Character.MIN_VALUE && rowArray[i+2] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (rowArray[i+1] == 'X' && rowArray[i+2] == 'X' && rowArray[i] == Character.MIN_VALUE && rowArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (rowArray[i+1] == 'X' && rowArray[i+3] == 'X' && rowArray[i] == Character.MIN_VALUE && rowArray[i+2] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (rowArray[i+2] == 'X' && rowArray[i+3] == 'X' && rowArray[i] == Character.MIN_VALUE && rowArray[i+1] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			
-
-
-
-		}
 		
-		for(int i = 0; i < game.getBoardSize() - 3; i++) { //is supposed to gather score of all possible columns of 4, might be a better way to implement this
-			colArray = game.getCol(i);
-			
-			
-			if(colArray[i] == 'O' && colArray[i+1] == 'O' &&colArray[i+2] == 'O' &&colArray[i+3] == 'O') {
-				playerOScore += 10000;
-			}
-			if (colArray[i] == 'X' && colArray[i+1] == 'X' &&colArray[i+2] == 'X' &&colArray[i+3] == 'X') {
-				playerXScore += 5000;
-			}
-			if (colArray[i] == 'O' &&colArray[i+2] == 'O' &&colArray[i+3] == 'O' && colArray[i+1] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (colArray[i] == 'O' &&colArray[i+1] == 'O' &&colArray[i+3] == 'O' && colArray[i+2] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (colArray[i] == 'O' &&colArray[i+1] == 'O' &&colArray[i+2] == 'O' && colArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (colArray[i+1] == 'O' &&colArray[i+2] == 'O' &&colArray[i+3] == 'O' && colArray[i+1] == Character.MIN_VALUE) {
-				playerOScore += 500;
-			}
-			if (colArray[i] == 'O' && colArray[i+1] == 'O'&& colArray[i+2] == Character.MIN_VALUE && colArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (colArray[i] == 'O' && colArray[i+2] == 'O' && colArray[i+1] == Character.MIN_VALUE && colArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (colArray[i] == 'O' && colArray[i+3] == 'O' && colArray[i+1] == Character.MIN_VALUE && colArray[i+2] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (colArray[i+1] == 'O' && colArray[i+2] == 'O' && colArray[i] == Character.MIN_VALUE && colArray[i+3] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (colArray[i+1] == 'O' && colArray[i+3] == 'O' && colArray[i] == Character.MIN_VALUE && colArray[i+2] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
-			if (colArray[i+2] == 'O' && colArray[i+3] == 'O' && colArray[i] == Character.MIN_VALUE && colArray[i+1] == Character.MIN_VALUE) {
-				playerOScore += 50;
-			}
+	//	for(int i = 0; i < game.getBoardSize() - 3; i++) { 
+		//	if(game.getBoardAt(i, 3) == 88) {
+		//		playerXScore += 1000;
+		//}
+		//}
 
-			if (colArray[i] == 'X' &&colArray[i+2] == 'X' &&colArray[i+3] == 'X' && colArray[i+1] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (colArray[i] == 'X' &&colArray[i+1] == 'X' &&colArray[i+3] == 'X' && colArray[i+2] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (colArray[i] == 'X' &&colArray[i+1] == 'X' &&colArray[i+2] == 'X' && colArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (colArray[i+1] == 'X' &&colArray[i+2] == 'X' &&colArray[i+3] == 'X' && colArray[i+1] == Character.MIN_VALUE) {
-				playerXScore += 500;
-			}
-			if (colArray[i] == 'X' && colArray[i+1] == 'X'&& colArray[i+2] == Character.MIN_VALUE && colArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (colArray[i] == 'X' && colArray[i+2] == 'X' && colArray[i+1] == Character.MIN_VALUE && colArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (colArray[i] == 'X' && colArray[i+3] == 'X' && colArray[i+1] == Character.MIN_VALUE && colArray[i+2] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (colArray[i+1] == 'X' && colArray[i+2] == 'X' && colArray[i] == Character.MIN_VALUE && colArray[i+3] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (colArray[i+1] == 'X' && colArray[i+3] == 'X' && colArray[i] == Character.MIN_VALUE && colArray[i+2] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			if (colArray[i+2] == 'X' && colArray[i+3] == 'X' && colArray[i] == Character.MIN_VALUE && colArray[i+1] == Character.MIN_VALUE) {
-				playerXScore += 50;
-			}
-			
-		}
-		*/
-		bestScore = playerXScore - playerOScore ;
+		bestScore = playerOScore - playerXScore ;
+		System.out.println(bestScore);
+		
 		return bestScore;
-		
-	}
+		}
+	
 	public int evaluate(Connect4Core game) {
-		int score = -10078; //temp value
+		int score;//temp value
 		score = score(game);
 		return score;
 }
@@ -222,8 +81,8 @@ public class Connect4AI {
 	 * @return an int representing the column the move will be placed in.
 	 */
 	public int minimax(int depth, Connect4Core game, boolean isMax, int alpha, int beta) { 
-		if(depth == difficulty) {
-			bestScore = evaluate(game);
+		if(depth == difficulty || game.checkForWin() == true) {
+			//bestScore = evaluate(game);
 			return evaluate(game);
 		}
 		
@@ -237,11 +96,12 @@ public class Connect4AI {
 						 game.setCurrentPlayer('O');
 						 score = minimax(depth + 1, game, false, alpha, beta);
 						 game.undoMove(i,j);
-						 if(score > maxScore) {
-							 maxScore = score;
-							 maxMove = j;
+						 if(score > maxScore) {	
+							maxScore = score;
+						 	bestMove = j;
 						 	}
-						 alpha = Math.max(alpha, maxScore);
+
+						 alpha = Math.max(alpha, score);
 						 if(beta <= alpha){
 							 break;
 						 }
@@ -251,7 +111,7 @@ public class Connect4AI {
 
 				}
 
-			 return maxMove;
+			 return maxScore;
 			 }
 			 
 		
@@ -264,11 +124,13 @@ public class Connect4AI {
 							 game.setCurrentPlayer('X');
 							 score = minimax(depth + 1, game, true, alpha, beta);
 							 game.undoMove(i,j);
-							 if(score < minScore) {
-								 minScore = score;
-								 minMove = j;
+							 	if(score < minScore) {
+							 		minScore = score;
+							 		minMove = j;
 							 }
-							 beta = Math.min(beta, minScore);
+
+							 
+							 beta = Math.min(beta, score);
 							 if(beta <= alpha) {
 								 break;
 							 }
@@ -277,7 +139,7 @@ public class Connect4AI {
 
 				}
 			}
-		return minMove;
+		return minScore;
 		}
 }
 	/**
@@ -290,19 +152,21 @@ public class Connect4AI {
 		int move = 5;
 		Connect4Core copy = new Connect4Core(game);
 		
-		move = minimax(0, copy, true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		move = minimax(0, copy, true, Integer.MIN_VALUE, Integer.MAX_VALUE); 
 		//System.out.println(bestScore + "move" + move);
-	
+		//System.out.println("MOVE IS    " + move);
+		//System.out.println("BESTMOVE IS" + bestMove);
 		
 		//System.out.println(move); 
-		if(maxScore < 0|| move > game.getBoardSize() || game.isAllowed(move) == false) { //randomly picks a column, currently unused
-		move = rng.nextInt(game.getBoardSize());
-		while(game.isAllowed(move) == false) {
-			move = rng.nextInt(game.getBoardSize());
-		}
-		System.out.println("rng used!!!!!");
-		}
-		return move;
+		//if(bestMove < 0|| move > game.getBoardSize() || game.isAllowed(move) == false) { //randomly picks a column, currently unused
+		//move = rng.nextInt(game.getBoardSize());
+	//	while(game.isAllowed(move) == false) {
+		//	move = rng.nextInt(game.getBoardSize());
+	//	}
+		
+		//System.out.println("rng used!!!!!");
+		//}
+		return bestMove;
 	}
 }
 
